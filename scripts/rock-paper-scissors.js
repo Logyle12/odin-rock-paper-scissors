9b2497ed194426d3cc5a898bcc3c9bb41fd8cf9d
Logyle12 <135021553+Logyle12@users.noreply.github.com>
2024-10-12 19:07:08 +0300

    feat(js): complete logic for determining Rock, Paper, Scissors choice

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,3 +1,16 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 2);
+
+    if (computerChoice == 0) {
+        return "Rock";
+    }
+
+    else if (computerChoice == 1) {
+        return "Paper";
+    }
+
+    else {
+        return "Scissors"
+    }
+
 }
